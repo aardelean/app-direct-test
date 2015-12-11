93b9eaf4e057a6004d3fb8fb55d976e631822545
aardelean <alex_i_ardelean@yahoo.com>
2015-12-11 07:42:26 +0100

    spring security

--- src/main/java/home/app/direct/Starter.java
+++ src/main/java/home/app/direct/Starter.java
@@ -10,4 +10,5 @@ public class Starter  extends SpringBootServletInitializer implements WebApplica
     public static void main(String...args){
         SpringApplication.run(Starter.class);
     }
+
 }
---
+++ src/main/java/home/app/direct/WebApplicationConfiguration.java
@@ -0,0 +1,15 @@
+package home.app.direct;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebApplicationConfiguration extends WebMvcConfigurerAdapter{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
